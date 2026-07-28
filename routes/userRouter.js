8e5e716874f508aeb3030075903702386ab86e6c
elias-kodehode <elias96.kodehode@gmail.com>
2026-07-28 12:10:45 +0200

    .

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,23 +1,27 @@
 import express from "express";
 import * as userStore from "./stores/userStore.js"
+
 const router = express.Router();
 
 
-
+//get user by id
 router.get("/:id", (req, res) => {
     const id = req.params?.id;
     var found = userStore.getUserById(id);
-   res.json(found); 
+    res.json(found); 
 });
 
+//get all users
 router.get("/", (req, res) => {
     res.json({
         users: [...userStore.getAllUsers()]
     });
 });
 
+//delete user
 router.delete("/", (req, res) => {});
 
+//Create user
 router.post("/", (req, res) => {
     const { username, email } = req.body;
     const id = userStore.addUser({
